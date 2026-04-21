2a92bbfdfcc473e74405cf2c0498cd8b2f55110b
jortiz6929-lab <jortiz6929@gmail.com>
2026-04-21 11:25:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 "use strict";
-/**
- * @type {HTMLFormElement}
- */
+
 const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
 const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
 const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
 const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
 const errorCode = document.getElementById("uv-error-code");
-const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
 
 form.addEventListener("submit", async (event) => {
-	event.preventDefault();
+  event.preventDefault();
 
-	try {
-		await registerSW();
-	} catch (err) {
-		error.textContent = "Failed to register service worker.";
-		errorCode.textContent = err.toString();
-		throw err;
-	}
-
-	const url = search(address.value, searchEngine.value);
-
-	let frame = document.getElementById("uv-frame");
-	frame.style.display = "block";
-	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-	if (await connection.getTransport() !== "/epoxy/index.mjs") {
-		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-	}
-	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+  try {
+    // This uses the search logic from search.js
+    const url = search(address.value, searchEngine.value);
+    
+    // This redirects you into the "cloaked" proxy environment
+    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  } catch (err) {
+    error.textContent = "Module failed to load. Please try again.";
+    errorCode.textContent = err.toString();
+  }
 });
