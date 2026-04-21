63bbd9c3a2b4a6f643811c1390e5eecc6a61990e
jortiz6929-lab <jortiz6929@gmail.com>
2026-04-21 12:08:10 -0400

    Update uv.bundle.js

--- uv.bundle.js
+++ uv.bundle.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@latest/dist/uv.bundle.js');
+<script src="https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.bundle.js"></script>
