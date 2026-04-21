09a8cf28f262f878dfa7a914591c8fd0bfc1fb8b
jortiz6929-lab <jortiz6929@gmail.com>
2026-04-21 12:14:18 -0400

    Update uv.sw.js

--- uv.sw.js
+++ uv.sw.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@latest/dist/uv.sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.0.2/dist/uv.sw.js');
